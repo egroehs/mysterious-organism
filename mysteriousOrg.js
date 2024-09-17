7d286242298802730e7f67240645337aa53720d9
egroehs <eduardagroehs@gmail.com>
2024-09-17 12:46:03 -0700

    pAequorFactory

---
+++ mysteriousOrg.js
@@ -0,0 +1,35 @@
+// Returns a random DNA base
+const returnRandBase = () => {
+  const dnaBases = ["A", "T", "C", "G"];
+  return dnaBases[Math.floor(Math.random() * 4)];
+};
+
+// Returns a random single strand of DNA containing 15 bases
+const mockUpStrand = () => {
+  const newStrand = [];
+  for (let i = 0; i < 15; i++) {
+    newStrand.push(returnRandBase());
+  }
+  return newStrand;
+};
+
+function pAequorFactory(id, dnaArr) {
+  return {
+    specimenNum: id,
+    dna: dnaArr,
+
+    mutate() {
+      const randomIndex = Math.floor(Math.random() * 15);
+
+      const currentBase = this.dna[randomIndex];
+
+      let newBase = returnRandBase();
+      while (newBase === currentBase) {
+        newBase = returnRandBase();
+      }
+
+      this.dna[randomIndex] = newBase;
+      return this.dna;
+    },
+  };
+}
